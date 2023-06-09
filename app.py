21aebcda3339d759932074c45cf63cdfbc4bd0cd
hessdale <hessdale96@gmail.com>
2023-06-08 20:21:11 -0600

    trying out checking info before sending to api

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ import dbhelper
 from flask import Flask, request
 app = Flask(__name__)
 
-
+@app.post('/api/new_client')
+def new_client():
+    valid_info = dbhelper.check_endpoint_info(request.json,['username','password','premium'])
+    if(type(valid_info) == str):
+        return valid_info
 
 
 
