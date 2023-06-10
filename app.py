a0c1f03e75636ef33920b76642ebb142477cf707
hessdale <hessdale96@gmail.com>
2023-06-10 13:40:28 -0600

    messing around with dict to convert response data

--- app.py
+++ app.py
@@ -2,22 +2,54 @@ import json
 import mariadb
 import dbcreds
 import dbhelper
-from flask import Flask, request
+from flask import Flask, request, make_response, jsonify
 app = Flask(__name__)
 
-@app.post('/api/new_client')
+
+@app.post('/api/client')
 def new_client():
-    valid_info = dbhelper.check_endpoint_info(request.json,['username','password','premium'])
-    if(type(valid_info) == str):
-        return valid_info
+    error = dbhelper.check_endpoint_info(request.json,['username','password','premium'])
+    if(error != None):
+        return make_response(jsonify(error),400)
     username = request.json.get('username')
     password = request.json.get('password')
     premium = request.json.get('premium')
     results =  dbhelper.run_procedure('call new_client(?,?,?)'[username,password,premium])
     if(type(results) == list):
+        return make_response(jsonify(results),200)
+    else:
+        return make_response('sorry something went wrong',500)
+
+@app.patch('/api/client')
+def update_password():
+    valid_info = dbhelper.check_endpoint_info(request.json,['username','old_password','new_password'])
+    if(type(valid_info) == str):
+        return valid_info
+    username = request.json.get('username')
+    old_password = request.json.get('old_password')
+    new_password = request.json.get('new_password')
+    results =  dbhelper.run_procedure('call update_password(?,?,?)'[username,old_password,new_password])
+    if(type(results) == list):
         return json.dumps(results,default=str)
     else:
         return 'sorry something went wrong'
+
+
+
+@app.get('/api/client')
+def get_all():
+    try:
+        results = dbhelper.run_procedure('call get_all()',[])
+        if(type(results) == list):
+            return json.dumps(results,default=str)
+        else:
+            return 'sorry something went wrong'
+    except TypeError:
+        print('invalid input type, try again.')
+    except UnboundLocalError:
+        print('coding error')
+    except ValueError:
+        print('value error, try again') 
 
 
 
--- dbhelper.py
+++ dbhelper.py
@@ -1,5 +1,13 @@
 import mariadb
 import dbcreds
+
+def convert_data(cursor,results):
+    column_names = [i[0] for i in cursor.description]
+    new_results = []
+    for row in results:
+        new_results.append(dict(zip(column_names,row)))
+        return new_results
+
 def run_procedure(sql,args):
     try:
         results = None
@@ -7,6 +15,7 @@ def run_procedure(sql,args):
         cursor = conn.cursor()
         cursor.execute(sql,args)
         results = cursor.fetchall()
+        results = convert_data(cursor,results)
     except mariadb.ProgrammingError as error:
         print('there is an issue with the db code: ',error)
     except mariadb.OperationalError:
